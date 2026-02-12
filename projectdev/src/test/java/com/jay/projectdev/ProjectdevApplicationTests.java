3f465eaa7a6a7c995df72b77f01e6b57d81daa31
reginjacobciubal <reginjacob.ciubal@lorma.edu>
2026-02-12 16:52:04 +0800

    Add files via upload
    Exam prelims review

---
+++ projectdev/src/test/java/com/jay/projectdev/ProjectdevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jay.projectdev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectdevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
